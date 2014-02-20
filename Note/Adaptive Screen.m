8230733940ec4bfbb5f711b5d252b892a0c7760e
yume190 <yume190@gmail.com>
2014-02-20 13:46:08 +0800

    Update Adaptive Screen.m

--- Note/Adaptive Screen.m
+++ Note/Adaptive Screen.m
@@ -10,9 +10,9 @@ Adaptive Screen
 
 
     float currSysVer = [[[UIDevice currentDevice] systemName] floatValue];
-    float statusBar = 0.0;  //IOS 6
+    float statusBar = 0.0;      //IOS 6
     if(currSysVer >= 7.0){
-        statusBar = 20; //IOS 7
+        statusBar = 20;         //IOS 7
     }
     CGSize size = [UIScreen mainScreen].bounds.size;
     CGRect newTextViewFrame = CGRectMake(0, 0 + statusBar, size.width, size.height - statusBar - 44);
