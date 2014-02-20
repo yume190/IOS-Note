358a12fd37fb75305c74a83a29bbede416e9006e
yume190 <yume190@gmail.com>
2014-02-20 13:44:19 +0800

    add Adaptive Sreen.m
    ...

---
+++ Note/Adaptive Screen.m
@@ -0,0 +1,21 @@
+Adaptive Screen
+========
+
+ *          width  height
+ * screen    320    
+
+
+
+
+
+
+    float currSysVer = [[[UIDevice currentDevice] systemName] floatValue];
+    float statusBar = 0.0;  //IOS 6
+    if(currSysVer >= 7.0){
+        statusBar = 20; //IOS 7
+    }
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    CGRect newTextViewFrame = CGRectMake(0, 0 + statusBar, size.width, size.height - statusBar - 44);
+    
+
+
