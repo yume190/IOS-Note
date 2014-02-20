e806362fd45bb769e92c008c79c827829352dbad
yume190 <yume190@gmail.com>
2014-02-20 16:14:27 +0800

    Update Adaptive Screen.m

--- Note/Adaptive Screen.m
+++ Note/Adaptive Screen.m
@@ -12,5 +12,19 @@
     CGSize size = [UIScreen mainScreen].bounds.size;
     CGRect newTextViewFrame = CGRectMake(0, 0 + statusBar, size.width, size.height - statusBar - 44);
     
+////////////////////////////////////////////////////////////////////////////
+//創建新的模擬位置
+//new file > resource > GPX file
 
+////////////////////////////////////////////////////////////////////////////
+//yumeMyAnnotation.h 設定
+#import <MapKit/MapKit.h>
 
+@interface yumeMyAnnotation : NSObject<MKAnnotation>
+
+@property (nonatomic,readonly)CLLocationCoordinate2D coordinate;
+@property (nonatomic,readonly,copy)NSString *title;
+@property (nonatomic,readonly,copy)NSString *subTitle;
+
+-(id)initWithTitle:(NSString *)theTitle andSubTitle:(NSString*)theSubTitle andCoordinate:(CLLocationCoordinate2D)theCoordinate;
+//
